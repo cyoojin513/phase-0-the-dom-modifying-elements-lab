179af81c5fabf00127d078e04c02c8895b1855be
yoojin <yoojin9005@gmail.com>
2022-06-28 11:12:17 -0400

    commit tree 0b6369edf6c1990b83f4bd0d131765b2b07a1454

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const main = document.getElementById("main");
-main.remove();
+document.getElementById("main").remove()
 
-const newHeader = document.createElement('h1');
+const newHeader = document.createElement("h1")
 newHeader.id = "victory"
 
-newHeader.textContent = "YOUR-NAME is the champion";
+newHeader.textContent = "YOUR-NAME is the champion"
 
-document.body.append(newHeader);
+document.body.append("h1")
